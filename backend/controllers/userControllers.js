2cb8503c1e30480220091a6ef284a55bb04f6f9a
NawazT <nawaztausif63@gmail.com>
2023-10-28 01:50:47 +0530

    Login authentication and json web token added

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler')
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 const bcrypt = require('bcryptjs')
 
@@ -41,6 +42,7 @@ const registerUser = asyncHandler(async(req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
+            token: getToken(user._id),
         })
     } else {
         res.status(400)
@@ -53,8 +55,34 @@ const registerUser = asyncHandler(async(req, res) => {
 // route -        /api/users/login
 // access type -  Public
 const loginUser = asyncHandler(async(req, res) => {
+
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password)))
+    {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: getToken(user._id),
+        })
+    }
+    else
+    {
+        res.status(401)
+        throw new Error('Invalid credentials')
+    }
     res.send('Login User')
 })
+
+//Get json Web Token
+const getToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '60d'
+    })
+}
 
 module.exports = {
     registerUser,
